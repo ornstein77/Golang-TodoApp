e020c2ff5179462077fe5f7e8e455541699d5dbb
Antonio <dimon085496@gmail.com>
2026-06-06 18:27:46 +1000

    add feature tasks

---
+++ internal/features/tasks/transport/http/delete_task.go
@@ -0,0 +1,31 @@
+package tasks_transport_http
+
+import (
+	"net/http"
+
+	core_logger "github.com/ornstein77/Golang-TodoApp/internal/core/logger"
+	core_http_request "github.com/ornstein77/Golang-TodoApp/internal/core/transport/http/request"
+	core_http_response "github.com/ornstein77/Golang-TodoApp/internal/core/transport/http/response"
+)
+
+func (h *TasksHTTPHandler) DeleteTask(rw http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log := core_logger.FromContext(ctx)
+	responseHandler := core_http_response.NewHTTPResponseHandler(log, rw)
+
+	taskID, err := core_http_request.GetIntPathValue(r, "id")
+	if err != nil {
+		responseHandler.ErrorResponse(
+			err,
+			"failed to get ttaskID path value",
+		)
+		return
+	}
+	if err := h.tasksService.DeleteTask(ctx, taskID); err != nil {
+		responseHandler.ErrorResponse(
+			err,
+			"failed to delete task",
+		)
+		return
+	}
+}
