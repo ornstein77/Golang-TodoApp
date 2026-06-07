ebbf956a2c549a6da5755e97f46d021814634343
Antonio <dimon085496@gmail.com>
2026-06-08 04:32:13 +1000

    statistics

---
+++ internal/features/statistics/service/get_statistics.go
@@ -0,0 +1,74 @@
+package statistics_service
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ornstein77/Golang-TodoApp/internal/core/domain"
+	core_errors "github.com/ornstein77/Golang-TodoApp/internal/core/errors"
+)
+
+func (s *StatisticsService) GetStatistics(
+	ctx context.Context,
+	userID *int,
+	from *time.Time,
+	to *time.Time,
+) (domain.Statistics, error) {
+	if from != nil && to != nil {
+		if to.Before(*from) || to.Equal(*from) {
+			return domain.Statistics{}, fmt.Errorf(
+				"`to` must ber after `from`: %w",
+				core_errors.ErrInvalidArgument,
+			)
+		}
+	}
+
+	tasks, err := s.statisticsRepository.GetTasks(ctx, userID, from, to)
+	if err != nil {
+		return domain.Statistics{}, fmt.Errorf("get tasks from repository: %w", err)
+	}
+
+	statistics := calcStatistics(tasks)
+	return statistics, nil
+
+}
+
+func calcStatistics(tasks []domain.Task) domain.Statistics {
+	if len(tasks) == 0 {
+		return domain.NewSatistics(0, 0, nil, nil)
+
+	}
+
+	tasksCreated := len(tasks)
+
+	tasksCompleted := 0
+	var totalCompletiondDuration time.Duration
+	for _, task := range tasks {
+		if task.Completed {
+			tasksCompleted++
+		}
+
+		completionDuration := task.CompletionDuration()
+		if completionDuration != nil {
+			totalCompletiondDuration += *completionDuration
+		}
+	}
+
+	tasksCompletedRate := float64(tasksCompleted) / float64(tasksCreated) * 100
+
+	var tasksAverageCompletionTime *time.Duration
+	if tasksCompleted > 0 && totalCompletiondDuration != 0 {
+		avg := totalCompletiondDuration / time.Duration(tasksCompleted)
+
+		tasksAverageCompletionTime = &avg
+	}
+
+	return domain.NewSatistics(
+		tasksCreated,
+		tasksCompleted,
+		&tasksCompletedRate,
+		tasksAverageCompletionTime,
+	)
+
+}
